8c41d178dc42be6d45089ac5baabc0fb93595338
Taswoor2507 <taswoorhossein72@gmail.com>
2024-06-04 16:13:50 +0500

    add limiter as a middleware

--- src/app.js
+++ src/app.js
@@ -24,6 +24,8 @@ app.use(cors({
   origin: '*'
 }));
 
+app.use(limiter); // Apply limiter to all routes
+
 // Built-in middlewares
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ limit: '16kb', extended: 'true' }));
